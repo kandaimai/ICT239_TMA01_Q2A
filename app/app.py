f25839ee2c64d0787b6fd222685c2081fba922e9
kandaimai <daimai.kan@gmail.com>
2025-10-11 11:13:01 +0800

    2

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user, login_required
 from books import all_books  # Import the all_books list from books.py
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='assets', static_url_path='/assets')
 
 @app.route('/books')
 def list_books():
