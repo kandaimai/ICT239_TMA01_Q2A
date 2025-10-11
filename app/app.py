76ff7ce7a44226fb6015d4a1ba245e06d26503f8
kandaimai <daimai.kan@gmail.com>
2025-10-11 16:39:42 +0800

    3

--- app/app.py
+++ app/app.py
@@ -4,7 +4,20 @@ from books import all_books  # Import the all_books list from books.py
 
 app = Flask(__name__, static_folder='assets', static_url_path='/assets')
 
-@app.route('/books')
+@app.route('/', methods = ['GET', 'POST'])
 def list_books():
-    return render_template('base.html', books=all_books)
+    if request.method == 'GET':
+        return render_template('book_list.html', books=all_books)
+    elif request.method == 'POST':
+        cat = request.form['cat']
+        filtered_books = [book for book in all_books if book['category'] ==  cat]
+        #return cat
+        return render_template('book_list.html', books=filtered_books)
 
+'''
+@app.route('/book_cat/', methods = ['GET', 'POST'])
+def list_book_cat(cat):
+
+    return render_template('book_list.html', books=all_books, cat = cat)
+
+'''
